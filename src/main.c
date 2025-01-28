9da9bcad69d4f73e1a99e8aa89e065332302cb64
Bennett <69775498+supercat3point0@users.noreply.github.com>
2025-01-27 16:11:56 -0800

    More set up

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
   gtk_window_set_title(GTK_WINDOW(window), "Foobar");
   gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
-  button = gtk_button_new_with_label("Hello World");
+  button = gtk_button_new_with_label("Click Me");
   g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
   gtk_window_set_child(GTK_WINDOW(window), button);
 
