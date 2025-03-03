5786cb5a49a00dfda03162e84ab81122a95502dc
Bennett <69775498+supercat3point0@users.noreply.github.com>
2025-03-02 17:45:09 -0800

    Implement carbon footprint calculations

--- src/main.c
+++ src/main.c
@@ -1,28 +1,62 @@
 #include <stdlib.h>
+#include <stdio.h>
+
 #include <gtk/gtk.h>
 
-static void activate(GtkApplication *app) {
-  GtkBuilder *build;
-  GtkApplicationWindow *window;
+static GtkBuilder *build;
 
+G_MODULE_EXPORT void calculate_footprint(void) {
+  double footprint = 0;
+
+  GtkSpinButton *electric = GTK_SPIN_BUTTON(gtk_builder_get_object(build, "electric"));
+  footprint += gtk_spin_button_get_value(electric) * 105;
+
+  GtkSpinButton *gas = GTK_SPIN_BUTTON(gtk_builder_get_object(build, "gas"));
+  footprint += gtk_spin_button_get_value(gas) * 105;
+
+  GtkSpinButton *oil = GTK_SPIN_BUTTON(gtk_builder_get_object(build, "oil"));
+  footprint += gtk_spin_button_get_value(oil) * 113;
+
+  GtkSpinButton *mileage = GTK_SPIN_BUTTON(gtk_builder_get_object(build, "mileage"));
+  footprint += gtk_spin_button_get_value(mileage) * 0.79;
+
+  GtkSpinButton *flights = GTK_SPIN_BUTTON(gtk_builder_get_object(build, "flights"));
+  footprint += gtk_spin_button_get_value(flights) * 1100;
+
+  GtkSpinButton *flights4 = GTK_SPIN_BUTTON(gtk_builder_get_object(build, "flights4"));
+  footprint += gtk_spin_button_get_value(flights4) * 4400;
+
+  GtkCheckButton *newspaper = GTK_CHECK_BUTTON(gtk_builder_get_object(build, "newspaper"));
+  if (!gtk_check_button_get_active(newspaper)) footprint += 184;
+
+  GtkCheckButton *aluminum = GTK_CHECK_BUTTON(gtk_builder_get_object(build, "aluminum"));
+  if (!gtk_check_button_get_active(aluminum)) footprint += 166;
+
+  char *str = malloc((snprintf(NULL, 0, "Carbon footprint: %glbs", footprint) + 1) * sizeof(char));
+  sprintf(str, "Carbon footprint: %glbs", footprint);
+  GtkLabel *label = GTK_LABEL(gtk_builder_get_object(build, "footprint"));
+  gtk_label_set_text(label, str);
+  free(str);
+}
+
+static void activate(GtkApplication *app) {
   build = gtk_builder_new_from_resource("/net/catech-software/climate-tracker/climate-tracker.ui");
 
-  window = GTK_APPLICATION_WINDOW(gtk_builder_get_object(build, "window"));
+  GtkApplicationWindow *window = GTK_APPLICATION_WINDOW(gtk_builder_get_object(build, "window"));
   gtk_window_set_application(GTK_WINDOW(window), app);
 
-  g_object_unref(build);
-
   gtk_window_present(GTK_WINDOW(window));
+
+  calculate_footprint();
 }
 
 int main(int argc, char *argv[]) {
-  GtkApplication *app;
-  int status;
-
-  app = gtk_application_new("net.catech_software.climate_tracker", G_APPLICATION_DEFAULT_FLAGS);
+  GtkApplication *app = gtk_application_new("net.catech_software.climate_tracker", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-  status = g_application_run(G_APPLICATION(app), argc, argv);
+  int status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
+
+  g_object_unref(build);
 
   return status;
 }
